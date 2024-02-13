87c70ad2107d6a9c3fedfcae8c5d71f488a48ee3
Tim Grekov <tgrekov@students.hive.fi>
2024-02-13 09:07:34 +0200

    I forgot to add the doxygen file comment somehow

--- ft_printf.h
+++ ft_printf.h
@@ -6,9 +6,16 @@
 /*   By: tgrekov <tgrekov@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 23:08:39 by tgrekov           #+#    #+#             */
-/*   Updated: 2024/02/11 23:30:11 by tgrekov          ###   ########.fr       */
+/*   Updated: 2024/02/13 09:06:38 by tgrekov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/**
+ * @file ft_printf.h
+ * @dontinclude ft_printf.h
+ * @line /\* *********
+ * @until /\* *********
+ */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
